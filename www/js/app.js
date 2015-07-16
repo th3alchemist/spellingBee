f448314d7303878aa5f2819d7369a3f62e3d5fb2
th3alchemist <ayo.j.arum@gmail.com>
2015-07-16 14:20:58 -0400

    changer 'start' app to 'spellingBee'

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,10 @@
-// Ionic Starter App
+// Ionic spellingBee App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'spellingBee' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+// 'spellingBee.controllers' is found in controllers.js
+angular.module('spellingBee', ['ionic', 'spellingBee.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('spellingBee.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
