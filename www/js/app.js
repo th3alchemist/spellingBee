b2246393acabb61edb4868dbfb9353589ab9285c
th3alchemist <ayo.j.arum@gmail.com>
2015-07-16 14:31:27 -0400

    removed playlists controller

--- www/js/app.js
+++ www/js/app.js
@@ -41,11 +41,11 @@ angular.module('spellingBee', ['ionic', 'spellingBee.controllers'])
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
+  .state('app.main', {
+      url: '/main',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/main.html'
         }
       }
     })
